bff42d8b71a53e80b61ebfd9b2641f6c11241b45
Fredy Lievano <fredylg@hotmail.com>
2013-03-26 21:54:05 +1030

    with some html

--- WWW/templates/page.tpl
+++ WWW/templates/page.tpl
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="Description" content="{$page_metadescription}" />
-	<meta name="Keywords" content="{$page_metawords}" />
-	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />		
-	<meta name="Distribution" content="Global" />
-	<meta name="Robots" content="index,follow" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">	
-	<title>{$page_seo_title}</title>
-	<!-- <link href="/images/template/favicon.ico" type="image/x-icon" rel="shortcut icon"> -->
-	 <script src="http://code.jquery.com/jquery.js"></script>
-
-	{block name=head}{/block}
-
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="Description" content="{$page_metadescription}" />
+<meta name="Keywords" content="{$page_metawords}" />
+<meta name="Distribution" content="Global" />
+<meta name="Robots" content="index,follow" />
+<!-- css -->
+<link href='http://fonts.googleapis.com/css?family=Clicker+Script|Gabriela' rel='stylesheet' type='text/css'>
+<link href="css/bootstrap.css" rel="stylesheet"  /> 
+<link href="css/form.css" rel="stylesheet"  />
+<link href="css/ie.css" rel="stylesheet"  />
+<link href="css/jquery-ui-1.10.1.custom.css" rel="stylesheet"  /> 
+<link href="css/main.css" rel="stylesheet"  /> 
+<link href="css/screen.css" rel="stylesheet"  />
+<link href="css/styles.css" rel="stylesheet"  />
+<title>{$page_seo_title}</title>
+<!-- <link href="/images/template/favicon.ico" type="image/x-icon" rel="shortcut icon"> -->
+<script src="http://code.jquery.com/jquery.js"></script>
+{block name=head}{/block}
 </head>
 <body>
 	{block name=body}{/block}
--- WWW/templates_c/59981438c9471f7d55acbb089adb3789cec0ebe1.extends.staticpage.tpl.php
+++ WWW/templates_c/59981438c9471f7d55acbb089adb3789cec0ebe1.extends.staticpage.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.13, created on 2013-03-25 11:38:35
+<?php /* Smarty version Smarty-3.1.13, created on 2013-03-26 11:14:10
          compiled from "templates/staticpage.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:3222094405150373b00e2f1-19209771%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -13,7 +13,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'f0f109d05c64df629f4e796edc308cec65bb638f' => 
     array (
       0 => 'templates/page.tpl',
-      1 => 1364211505,
+      1 => 1364296433,
       2 => 'file',
     ),
   ),
@@ -21,6 +21,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.13',
+  'unifunc' => 'content_5150373b085e23_35319671',
   'variables' => 
   array (
     'page_metadescription' => 0,
@@ -28,27 +30,31 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'page_seo_title' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.13',
-  'unifunc' => 'content_5150373b085e23_35319671',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_5150373b085e23_35319671')) {function content_5150373b085e23_35319671($_smarty_tpl) {?>
-<!DOCTYPE html>
+<?php if ($_valid && !is_callable('content_5150373b085e23_35319671')) {function content_5150373b085e23_35319671($_smarty_tpl) {?><!DOCTYPE html>
 <html>
 <head>
-	<meta name="Description" content="<?php echo $_smarty_tpl->tpl_vars['page_metadescription']->value;?>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="Description" content="<?php echo $_smarty_tpl->tpl_vars['page_metadescription']->value;?>
 " />
-	<meta name="Keywords" content="<?php echo $_smarty_tpl->tpl_vars['page_metawords']->value;?>
+<meta name="Keywords" content="<?php echo $_smarty_tpl->tpl_vars['page_metawords']->value;?>
 " />
-	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />		
-	<meta name="Distribution" content="Global" />
-	<meta name="Robots" content="index,follow" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">	
-	<title><?php echo $_smarty_tpl->tpl_vars['page_seo_title']->value;?>
+<meta name="Distribution" content="Global" />
+<meta name="Robots" content="index,follow" />
+<!-- css -->
+<link href='http://fonts.googleapis.com/css?family=Clicker+Script|Gabriela' rel='stylesheet' type='text/css'>
+<link href="css/bootstrap.css" rel="stylesheet"  /> 
+<link href="css/form.css" rel="stylesheet"  />
+<link href="css/ie.css" rel="stylesheet"  />
+<link href="css/jquery-ui-1.10.1.custom.css" rel="stylesheet"  /> 
+<link href="css/main.css" rel="stylesheet"  /> 
+<link href="css/screen.css" rel="stylesheet"  />
+<link href="css/styles.css" rel="stylesheet"  />
+<title><?php echo $_smarty_tpl->tpl_vars['page_seo_title']->value;?>
 </title>
-	<!-- <link href="/images/template/favicon.ico" type="image/x-icon" rel="shortcut icon"> -->
-	 <script src="http://code.jquery.com/jquery.js"></script>
-
-	
+<!-- <link href="/images/template/favicon.ico" type="image/x-icon" rel="shortcut icon"> -->
+<script src="http://code.jquery.com/jquery.js"></script>
 
 </head>
 <body>
